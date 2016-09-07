f6cddbb0e5cb59b0dfeb6bc15c94d0693d1785c9
Oskar Ek <oskarek24@gmail.com>
2016-09-07 18:54:23 +0200

    main-metod, programmet säger Hej

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main-klassen för programmet.
+ * Created by oskarek on 2016-09-07.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hej");
+    }
+}
+
